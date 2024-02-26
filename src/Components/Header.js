9fb4f4260662319b88e029992fef73f346b37957
Akshay Kumar <114898081+akshay290202@users.noreply.github.com>
2024-02-27 00:59:33 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <nav>Developed with <span>&#10084;</span> by KING</nav>
+    <nav>Developed with <span>&#10084;</span> by Akshay</nav>
   )
 }
 
